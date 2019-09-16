c226a77e97c7205ffb08320f5fd40fd8dd60a396
Pedro Oliveira <pedrooliveira@ioasys.com.br>
2019-09-15 21:43:33 -0300

    configuring gradle dependencies

---
+++ app/src/main/java/com/pedro/db1/teste/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.pedro.db1.teste.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pedro.db1.teste.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
